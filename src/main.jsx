d73729ebe6a820199176263e645f87240c8fcc63
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-01-10 23:41:48 +0530

    "Updated main.jsx"

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 
-<<<<<<< HEAD
+
 // Method 1 
 // const router = createBrowserRouter([
 //   {
